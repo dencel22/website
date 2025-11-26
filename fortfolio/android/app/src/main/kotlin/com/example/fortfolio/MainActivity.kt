7210ea8dd8e0f1fc6b95af351e0a2aa8cf861b5c
Dens <dens@Denss-MacBook-Pro.local>
2025-11-26 14:47:25 +0800

    fix

--- fortfolio/android/app/src/main/kotlin/com/example/fortfolio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fortfolio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dev Dens',
       debugShowCheckedModeBanner: false,
       theme: ThemeWebsite.websiteTheme,
       home: const HomeScreen(),
